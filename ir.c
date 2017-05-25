7b92086237b66845687ec8d473ebf43fd10cf1a9
Pistachio <pistachio.us@hotmail.com>
2017-05-25 23:14:01 +0700

    add function push_ircode

--- ir.c
+++ ir.c
@@ -6,5 +6,50 @@
 #include "ir.h"
 
 void do_gen_ir(A_expList expList) {
-    
+    A_exp exp;
+    for(A_expList it = expList; it; it->next){
+        exp = it->exp;
+        assert(exp->kind == A_callExp);
+        gen_ic(it->exp);
+        if (strcmp(exp->u.call.func, "main") == 0){
+
+        } else {
+
+        }
+    }
+}
+
+IRCode create_ircode(enum IRInstruction iri, union IRVar u, IRExpression e1, IRCode *next) {
+    IRCode irc = malloc(sizeof(IRCode));
+    irc->iri = iri;
+    irc->u = u;
+    irc->e1 = e1;
+    irc->next = next;
+    return irc;
+}
+
+IRExpression create_irexpression(enum IROp irop, union IRVar e1, enum IRVar e1_type, 
+                                            union IRVar e2, enum IRVar e2_type) {
+    IRExpression ire = malloc(sizeof(IRExpression));
+    ire->irop = irop;
+    ire->e1 = e1;
+    ire->e1_type = e1_type;
+    ire->e2_type = e2_type;
+    ire->e2 = e2;
+}
+
+void push_ircode(IRCode **ircodes, enum IRInstruction iri, union IRVar u, IRExpression e1, IRCode *next) {
+    IRCode tmp = create_ircode(iri, u, e1, next);
+    _push_ircode(ircodes, tmp);
+}
+
+void _push_ircode(IRCode **ircodes, IRCode *ircode) {
+    if(!ircodes) return;
+    if(!*ircodes) { 
+        *ircodes = ircode;
+    } else {
+        IRCode *it = *ircodes;
+        while(it->next) it = it->next;
+        it->next = ircode;
+    }
 }
--- ir.h
+++ ir.h
@@ -2,7 +2,6 @@
 #define _IRCSUBSET_C_
 #define MAX_VAR_LENGTH 256
 
-
 extern enum IRInstruction {
     fjmp, tjmp, jmp, assign, array_assign, cjmp, array_access,
     field_access, addr_ins, copy, load, store
@@ -18,7 +17,7 @@ extern enum IROp {
     eq, neq, le, leq, ge, geq,
     minus, neg
 }
-union IRVar{
+union IRVar {
     long cons;
     char name[MAX_VAR_LENGTH];
     char label[MAX_VAR_LENGTH];
@@ -26,18 +25,21 @@ union IRVar{
 typedef struct IRCode {
     enum IRInstruction iri;
     union IRVar u;
-    IRExpression e1, e2;
+    IRExpression e1;
+    struct IRCode *next;
 } IRCode;
 
 typedef struct IRExpression {
     enum IROp irop;
     union IRVar e1;
+    enum IRVar e1_type;
     union IRVar e2;
-
+    enum IRVar e2_type;
 } IRExpression;
 
-extern IRCode create_ircode(enum IRData irdata, IRExpression *l, IRExpression *r);
-extern IRExpression create_irexpression(enum IROp irop);
+extern IRCode create_ircode(enum IRInstruction iri, IRExpression *l, IRExpression *r);
+extern void push_ircode(IRCode **ircodes, enum IRInstruction iri, union IRVar u, IRExpression e1, IRCode *next) 
+extern IRExpression create_irexpression(enum IROp irop, union IRVar e1, enum IRVar, union IRVar, enum IRVar);
 extern void do_gen_ir(A_expList expList);
 
 #endif
